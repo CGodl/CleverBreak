ba9dc5b15843dd608ac8507be7859044a9601648
CG <cgodlew@gmail.com>
2020-06-08 11:56:22 -0400

    Fixed merging conflict

--- app/mailers/new_friend_mailer.rb
+++ app/mailers/new_friend_mailer.rb
@@ -1,12 +1,12 @@
-class newFriendMailer < ApplicationMailer
+# class newFriendMailer < ApplicationMailer
 
 
-  def newFriend(user)
-    @user = user
-    @email_text = `Hello there, #{user} has just invited you to join Cleverbreak.`
-  end
+#   def newFriend(user)
+#     @user = user
+#     @email_text = `Hello there, #{user} has just invited you to join Cleverbreak.`
+#   end
   
 
 
 
-end
+# end
