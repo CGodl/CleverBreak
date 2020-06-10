47c498e519efe0dbad4fe1744d11682a14f523f4
CG <cgodlew@gmail.com>
2020-06-09 21:49:15 -0400

    added friend api and actions to project

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -5,21 +5,23 @@ class FriendsController < ApplicationController
     @friend.author_id = current_user.id
     @friend.user_id = params[:id]
     unless @follow.save
-      flash[:errors] = @follow.errors.full_messages
+      render json: ["Unable to establish friendship"]
     end
-    ###redirect to ???
   end
 
   def destroy
-    @friend = Friend.find(params[:id])
+    @friend = Friend.find(params[:user_id])
     @friend.destroy
-    ####redirect_to
   end
+
 
   def show
-    
-
+    if params[:id]
+      @friend = Friend.where(user_id: params[:author_id])
+    end
   end
+
+
 
   def index
     @friend = Friend.find_by(params[:id])
