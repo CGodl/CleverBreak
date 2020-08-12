540b8c1c72226e35e28183633c954950a32b85c6
CG <cgodlew@gmail.com>
2020-08-12 18:47:20 -0400

    Fixed spelling error for curUserBillIds

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -2,11 +2,10 @@ class Api::FriendsController < ApplicationController
   before_action :require_logged_in
   
   def create
+    friend_email = params[:friend][:email]
     
-    requested_friend = User.find_by(email: [:email])
-
-    # requested_friend = User.find_by(user_params[:email])
-    
+    requested_friend = User.find_by(email: friend_email)
+        
     already_friend = Friend.find_by({requestor_id: current_user.id, requested_id: requested_friend.id })
   
     if requested_friend && !already_friend
