1ede936731fd4688bfd532c7706213a35cbf3b23
CG <cgodlew@gmail.com>
2020-08-25 01:26:03 -0400

    Changed default url for actiomailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,8 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true  }
 
-    config.action_mailer.default_url_options = { host: "localhost:3000"}
+    # config.action_mailer.default_url_options = { host: "localhost:3000"}
+    config.action_mailer.default_url_options = { host: "https://cleverbreak.herokuapp.com/"}
 
 
 
